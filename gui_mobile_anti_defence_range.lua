e6f082a09acb9c9be09c037ea948ed2792c7dbc4
Alexander Lipatov <alexlpt@yandex.ru>
2015-02-15 18:32:13 +0300

    Updated by Floris
    changes are described here: https://springrts.com/phpbb/viewtopic.php?f=23&t=32964

--- gui_mobile_anti_defence_range.lua
+++ gui_mobile_anti_defence_range.lua
@@ -1,12 +1,12 @@
 --------------------------------------------------------------------------------
 function widget:GetInfo()
     return {
-        name      = "Mobile anti defence range v5",
-        desc      = "Draws circle to show mobile anti defence range",
-        author    = "[teh]decay",
-        date      = "5 oct 2013",
+        name      = "Anti Ranges",
+        desc      = "Draws circle to show anti defence ranges (options: /antiranges_glow, antiranges_fade)",
+        author    = "[teh]decay, Floris",
+        date      = "25 january 2015",
         license   = "GNU GPL, v2 or later",
-        version   = 5,
+        version   = 3,
         layer     = 5,
         enabled   = true  --  loaded by default?
     }
@@ -15,111 +15,188 @@ end
 -- project page on github: https://github.com/jamerlan/gui_mobile_anti_defence_range
 
 --Changelog
--- v2 [teh]decay Add water antinukes
--- v3 [teh]decay fix spectator mode
--- v4 [teh]decay fix spectator mode when /specfullview is used
--- v5 [teh]decay fix spectator mode again (thx to jK)
+-- v2 [teh]decay:  Add water antinukes
+-- v3 Floris:  added normal anti, changed widget name, optional glow, optional fadeout on closeup, changed line thickness and opacity, empty anti uses different color
+
+
+--------------------------------------------------------------------------------
+-- Console commands
+--------------------------------------------------------------------------------
+
+--/antiranges_glow		-- toggles a faint glow on the line
+--/antiranges_fade		-- toggles hiding of ranges when zoomed in
+
+--------------------------------------------------------------------------------
+-- Options
+--------------------------------------------------------------------------------
+
+local opacityMultiplier			= 0.85
+local multiStockpileColor		= {1,1,0}
+local filledStockpileColor		= {1,0.73,0}
+local emptyStockpileColor		= {1,0.28,0}
+local showLineGlow				= true
+local fadeOnCloseup        		= true
+local fadeStartDistance			= 3300
 
 --------------------------------------------------------------------------------
 -- Speedups
 --------------------------------------------------------------------------------
-local arm_mobile_anti = UnitDefNames.armscab.id
-local arm_mobile_anti_water = UnitDefNames.armcarry.id
-local core_mobile_anti = UnitDefNames.cormabm.id
-local core_mobile_anti_water = UnitDefNames.corcarry.id
 
-local spGetActiveCommand = Spring.GetActiveCommand
-local spGetMouseState = Spring.GetMouseState
-local spTraceScreenRay = Spring.TraceScreenRay
-local spPos2BuildPos = Spring.Pos2BuildPos
+local arm_anti					= UnitDefNames.armamd.id
+local arm_mobile_anti			= UnitDefNames.armscab.id
+local arm_mobile_anti_water		= UnitDefNames.armcarry.id
+local core_anti					= UnitDefNames.corfmd.id
+local core_mobile_anti			= UnitDefNames.cormabm.id
+local core_mobile_anti_water	= UnitDefNames.corcarry.id
 
-local glColor = gl.Color
-local glDepthTest = gl.DepthTest
-local glDrawGroundCircle = gl.DrawGroundCircle
+local spGetActiveCommand		= Spring.GetActiveCommand
+local spGetMouseState			= Spring.GetMouseState
+local spTraceScreenRay			= Spring.TraceScreenRay
+local spPos2BuildPos			= Spring.Pos2BuildPos
 
-
-local spGetMyPlayerID		= Spring.GetMyPlayerID
-local spGetPlayerInfo		= Spring.GetPlayerInfo
-local spGetMyAllyTeamID	= Spring.GetMyAllyTeamID
-local spGetUnitDefID	= Spring.GetUnitDefID
-local spGetUnitPosition = Spring.GetUnitPosition
-local spGetUnitVelocity = Spring.GetUnitVelocity
-local spMarkerAddPoint	= Spring.MarkerAddPoint
-local spGetTeamUnits	= Spring.GetTeamUnits
-local spGetPositionLosState = Spring.GetPositionLosState
-local spGetAllUnits     = Spring.GetAllUnits
-local spGetSpectatingState = Spring.GetSpectatingState
+local glColor					= gl.Color
+local glDepthTest				= gl.DepthTest
+local glLineWidth				= gl.LineWidth
+local glDrawGroundCircle		= gl.DrawGroundCircle
 
 
-local mobileAntiInLos = {}
-local mobileAntiOutOfLos = {}
+local spGetMyPlayerID			= Spring.GetMyPlayerID
+local spGetPlayerInfo			= Spring.GetPlayerInfo
+local spGetMyAllyTeamID			= Spring.GetMyAllyTeamID
+local spGetUnitDefID			= Spring.GetUnitDefID
+local spGetUnitPosition			= Spring.GetUnitPosition
+local spGetUnitVelocity			= Spring.GetUnitVelocity
+local spMarkerAddPoint			= Spring.MarkerAddPoint
+local spGetTeamUnits			= Spring.GetTeamUnits
+local spGetPositionLosState 	= Spring.GetPositionLosState
+local spGetCameraPosition		= Spring.GetCameraPosition
+local spGetUnitStockpile		= Spring.GetUnitStockpile
+local spGetAllUnits    			= Spring.GetAllUnits
+
+local antiInLos					= {}
+local antiOutLos				= {}
 
 
-local coverageRangeArm = WeaponDefs[UnitDefNames.armscab.weapons[1].weaponDef].coverageRange
-local coverageRangeCore = WeaponDefs[UnitDefNames.cormabm.weapons[1].weaponDef].coverageRange
-local coverageRangeArmWater = WeaponDefs[UnitDefNames.armcarry.weapons[1].weaponDef].coverageRange
-local coverageRangeCoreWater = WeaponDefs[UnitDefNames.corcarry.weapons[1].weaponDef].coverageRange
-
-local spectatorMode = false
-local notInSpecfullmode = false
+local coverageRangeArmStatic	= WeaponDefs[UnitDefNames.armamd.weapons[1].weaponDef].coverageRange
+local coverageRangeCoreStatic	= WeaponDefs[UnitDefNames.corfmd.weapons[1].weaponDef].coverageRange
+local coverageRangeArm			= WeaponDefs[UnitDefNames.armscab.weapons[1].weaponDef].coverageRange
+local coverageRangeCore			= WeaponDefs[UnitDefNames.cormabm.weapons[1].weaponDef].coverageRange
+local coverageRangeArmWater		= WeaponDefs[UnitDefNames.armcarry.weapons[1].weaponDef].coverageRange
+local coverageRangeCoreWater	= WeaponDefs[UnitDefNames.corcarry.weapons[1].weaponDef].coverageRange
 
 --------------------------------------------------------------------------------
 -- Callins
 --------------------------------------------------------------------------------
-function widget:DrawWorldPreUnit()
-    local _, specFullView, _ = spGetSpectatingState()
 
-    if not specFullView then
-        notInSpecfullmode = true
-    else
-        if notInSpecfullmode then
-            detectSpectatorView()
-        end
-        notInSpecfullmode = false
-    end
 
-    for uID, pos in pairs(mobileAntiInLos) do
+
+function widget:DrawWorld()
+    if Spring.IsGUIHidden() then return end
+	local camX, camY, camZ = spGetCameraPosition()
+    for uID, pos in pairs(antiInLos) do
         local x, y, z = spGetUnitPosition(uID)
-
+        
         if x ~= nil and y ~= nil and z ~= nil then
-            glColor(1, 1, 0, .6)
-            glDepthTest(true)
-            glDrawGroundCircle(x, y, z, pos.coverageRange, 256)
+			drawCircle(uID, pos.coverageRange, x, y, z, camX, camY, camZ)
         end
     end
 
-    for uID, pos in pairs(mobileAntiOutOfLos) do
-        local a, b, c = spGetPositionLosState(pos.x, pos.y, pos.z)
-        if b then
-            mobileAntiOutOfLos[uID] = nil
+    for uID, pos in pairs(antiOutLos) do
+        local LosOrRadar, inLos, inRadar = spGetPositionLosState(pos.x, pos.y, pos.z)
+        if inLos then
+            antiOutLos[uID] = nil
         end
     end
 
-    for uID, pos in pairs(mobileAntiOutOfLos) do
+    for uID, pos in pairs(antiOutLos) do
         if pos.x ~= nil and pos.y ~= nil and pos.z ~= nil then
-            glColor(1, 1, 0, .6)
-            glDepthTest(true)
-            glDrawGroundCircle(pos.x, pos.y, pos.z, pos.coverageRange, 256)
+			drawCircle(uID, pos.coverageRange, pos.x, pos.y, pos.z, camX, camY, camZ)
         end
     end
 end
 
+
+function drawCircle(uID, coverageRange, x, y, z, camX, camY, camZ)
+	local xDifference = camX - x
+	local yDifference = camY - y
+	local zDifference = camZ - z
+	local camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
+	
+	local lineWidthMinus = (camDistance/fadeStartDistance)
+	if lineWidthMinus > 2 then
+		lineWidthMinus = 2
+	end
+	local lineOpacityMultiplier = 1
+	if fadeOnCloseup then
+		lineOpacityMultiplier = (camDistance - fadeStartDistance) / 1800
+		if lineOpacityMultiplier > 1 then
+			lineOpacityMultiplier = 1
+		end
+	end
+	lineOpacityMultiplier = lineOpacityMultiplier * opacityMultiplier
+	
+	if lineOpacityMultiplier > 0 then
+		local numStockpiled,numStockpileQued,stockpileBuild = spGetUnitStockpile(uID)
+		local circleColor = emptyStockpileColor
+		if numStockpiled == 1 then
+			circleColor = filledStockpileColor
+		elseif numStockpiled > 1 then
+			circleColor = multiStockpileColor
+		end
+		
+		glDepthTest(true)
+		if showLineGlow then
+			glLineWidth(10)
+			glColor(circleColor[1],circleColor[2],circleColor[3], .016*lineOpacityMultiplier)
+			glDrawGroundCircle(x, y, z, coverageRange, 256)
+		end
+		glColor(circleColor[1],circleColor[2],circleColor[3], .5*lineOpacityMultiplier)
+		glLineWidth(3-lineWidthMinus)
+		glDrawGroundCircle(x, y, z, coverageRange, 256)
+	end
+end
+
+
 function widget:UnitEnteredLos(unitID)
-    if not spectatorMode then
-        processVisibleUnit(unitID)
-    end
+    processVisibleUnit(unitID)
 end
 
 function processVisibleUnit(unitID)
     local unitDefId = spGetUnitDefID(unitID);
-    if unitDefId == nil then return end
-
-    if unitDefId == arm_mobile_anti or unitDefId == core_mobile_anti or unitDefId == arm_mobile_anti_water or unitDefId == core_mobile_anti_water then
+    if unitDefId == arm_anti or unitDefId == core_anti or unitDefId == arm_mobile_anti or unitDefId == core_mobile_anti or unitDefId == arm_mobile_anti_water or unitDefId == core_mobile_anti_water then
         local x, y, z = spGetUnitPosition(unitID)
         local pos = {}
         pos["x"] = x
         pos["y"] = y
         pos["z"] = z
+
+        if unitDefId == arm_mobile_anti then
+            pos.coverageRange = coverageRangeArm
+        elseif unitDefId == arm_anti then
+            pos.coverageRange = coverageRangeArmStatic
+        elseif unitDefId == core_anti then
+            pos.coverageRange = coverageRangeCoreStatic
+        elseif unitDefId == arm_mobile_anti_water then
+            pos.coverageRange = coverageRangeArmWater
+        elseif unitDefId == core_mobile_anti then
+            pos.coverageRange = coverageRangeCore
+        else
+            pos.coverageRange = coverageRangeCoreWater
+        end
+
+        antiInLos[unitID] = pos
+        antiOutLos[unitID] = nil
+    end
+end
+
+function widget:UnitLeftLos(unitID)
+    local unitDefId = spGetUnitDefID(unitID);
+    if unitDefId == arm_anti or unitDefId == core_anti or unitDefId == arm_mobile_anti or unitDefId == core_mobile_anti or unitDefId == arm_mobile_anti_water or unitDefId == core_mobile_anti_water then
+        local x, y, z = spGetUnitPosition(unitID)
+        local pos = {}
+        pos["x"] = x or antiInLos[unitID].x
+        pos["y"] = y or antiInLos[unitID].y
+        pos["z"] = z or antiInLos[unitID].z
 
         if unitDefId == arm_mobile_anti then
             pos.coverageRange = coverageRangeArm
@@ -131,34 +208,8 @@ function processVisibleUnit(unitID)
             pos.coverageRange = coverageRangeCoreWater
         end
 
-        mobileAntiInLos[unitID] = pos
-        mobileAntiOutOfLos[unitID] = nil
-    end
-end
-
-function widget:UnitLeftLos(unitID)
-    if not spectatorMode then
-        local unitDefId = spGetUnitDefID(unitID);
-        if unitDefId == arm_mobile_anti or unitDefId == core_mobile_anti or unitDefId == arm_mobile_anti_water or unitDefId == core_mobile_anti_water then
-            local x, y, z = spGetUnitPosition(unitID)
-            local pos = {}
-            pos["x"] = x or mobileAntiInLos[unitID].x
-            pos["y"] = y or mobileAntiInLos[unitID].y
-            pos["z"] = z or mobileAntiInLos[unitID].z
-
-            if unitDefId == arm_mobile_anti then
-                pos.coverageRange = coverageRangeArm
-            elseif unitDefId == arm_mobile_anti_water then
-                pos.coverageRange = coverageRangeArmWater
-            elseif unitDefId == core_mobile_anti then
-                pos.coverageRange = coverageRangeCore
-            else
-                pos.coverageRange = coverageRangeCoreWater
-            end
-
-            mobileAntiOutOfLos[unitID] = pos
-            mobileAntiInLos[unitID] = nil
-        end
+        antiOutLos[unitID] = pos
+        antiInLos[unitID] = nil
     end
 end
 
@@ -175,36 +226,63 @@ function widget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
 end
 
 function widget:GameFrame(n)
-    for uID, _ in pairs(mobileAntiInLos) do
-        if not Spring.GetUnitDefID(uID) then
-            mobileAntiInLos[uID] = nil -- has died
+    for uID, _ in pairs(antiInLos) do
+        if not spGetUnitDefID(uID) then
+            antiInLos[uID] = nil -- has died
         end
     end
-end
-
-function widget:PlayerChanged(playerID)
-    detectSpectatorView()
-    return true
 end
 
 function widget:Initialize()
-    detectSpectatorView()
-    return true
+	checkAllUnits()
 end
 
-function detectSpectatorView()
+function widget:PlayerChanged(playerID)
+	checkAllUnits()
+end
+
+
+function checkAllUnits()
     local _, _, spec, teamId = spGetPlayerInfo(spGetMyPlayerID())
-
-    if spec then
-        spectatorMode = true
-    end
-
-    local visibleUnits = spGetAllUnits()
-    if visibleUnits ~= nil then
-        for _, unitID in ipairs(visibleUnits) do
-            processVisibleUnit(unitID)
-        end
+	
+	antiInLos				= {}
+	antiOutLos				= {}
+	
+	local allUnits = spGetAllUnits()
+    for _, unitID in ipairs(allUnits) do
+        processVisibleUnit(unitID)
     end
 end
 
 --------------------------------------------------------------------------------
+
+function widget:GetConfigData(data)
+    savedTable = {}
+    savedTable.showLineGlow			= showLineGlow
+    savedTable.fadeOnCloseup		= fadeOnCloseup
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    if data.showLineGlow ~= nil 		then  showLineGlow			= data.showLineGlow end
+    if data.fadeOnCloseup ~= nil 		then  fadeOnCloseup			= data.fadeOnCloseup end
+end
+
+function widget:TextCommand(command)
+    if (string.find(command, "antiranges_glow") == 1  and  string.len(command) == 15) then 
+		showLineGlow = not showLineGlow
+		if showLineGlow then
+			Spring.Echo("Anti Ranges:  Glow on")
+		else
+			Spring.Echo("Anti Ranges:  Glow off")
+		end
+	end
+    if (string.find(command, "antiranges_fade") == 1  and  string.len(command) == 15) then 
+		fadeOnCloseup = not fadeOnCloseup
+		if fadeOnCloseup then
+			Spring.Echo("Anti Ranges:  Fade-out on closeup enabled")
+		else
+			Spring.Echo("Anti Ranges:  Fade-out on closeup disabled")
+		end
+	end
+end
